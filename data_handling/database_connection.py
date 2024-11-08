f99094d21b00c5440bad9aebe648b56830133c39
poornakishore <purnakishore.avvari@gmail.com>
2024-11-08 20:08:54 +0530

    code changes on dataconnection

--- data_handling/database_connection.py
+++ data_handling/database_connection.py
@@ -6,14 +6,26 @@ from pymongo import MongoClient
 
 load_dotenv()
 
-connection_string = os.getenv('connection_string')
-client = None
+
+# client = None
 
 def load_weather_data(transformed_data):
     """ Load transformed data in MongoDB Database"""
+    connection_string = os.getenv('connection_string')
+
+    if not connection_string:
+        print("Error: MogoDB connection string is not set")
+        return
+    print(f"Received connection string of length: {len(connection_string)}")
+
+    if not connection_string.startswith("mongodb://") and not connection_string.startswith("mongodb+srv://"):
+        print("Error: Connection string does not start with 'mongodb://' or 'mongodb+srv://'")
+        return
+    
     try:
-        client = MongoClient(connection_string, server_api=pymongo.server_api.ServerApi(
-        version="1", strict=True, deprecation_errors=True))
+        client = MongoClient(connection_string)
+
+        print("MongoDB client created successfully")
 
         database = client['GitHubActions']
         collection = database['weatherdata']
@@ -24,6 +36,4 @@ def load_weather_data(transformed_data):
     except Exception as e:
         raise Exception("The following error occred :",e)
     
-    finally:
-        if client is not None:
-            client.close()
+
